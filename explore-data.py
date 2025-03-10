0ed37d10b295e4ed0c22dcc57e52cfd53ab2ed75
YacineTrousselle <yacinetrousselle@gmail.com>
2025-03-10 13:39:26 +0100

    sssshhhhheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeessssssssshhhhhhhhhhhh

---
+++ explore-data.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+
+def print_sep():
+    print('\n--------------------------------------------------------\n')
+
+
+def hist_per_col(data, col):
+    plt.figure(figsize=(10, 5))
+    sns.histplot(data[col], bins=50, color='red', alpha=0.5)
+
+    plt.title('Distribution des ' + col)
+    plt.xlabel('Montant des ' + col)
+    plt.ylabel('Nombre de clients')
+
+    plt.show()
+
+
+def cat_per_col(data, col):
+    cat = pd.Categorical(data[col])
+
+    print(f'Colonne {col}')
+    print(cat.describe())
+
+    categories = []
+    for cat in cat.categories:
+        categories.append(str(cat))
+
+    print(f'Valeurs presentes: {', '.join(categories)}')
+
+
+if __name__ == '__main__':
+    demissionaires = pd.read_csv('donnees_banque/demissionaires.csv')
+    societaires = pd.read_csv('donnees_banque/societaires.csv')
+
+    dem_columns = list(demissionaires.columns)
+    soc_columns = list(societaires.columns)
+
+    print(f'Nombre de demissionaires: {len(demissionaires)}')
+    print(f'Nombre de societaires: {len(societaires)}')
+
+    print_sep()
+
+    print(f'Colonne de demissionaires: {dem_columns}')
+    print(f'Colonne de societaires: {soc_columns}')
+    print('Colonne en commun: ' + str([col for col in dem_columns if col in soc_columns]))
+
+    print_sep()
+
+    print(demissionaires.describe())
+    print(societaires.describe())
+
+    print_sep()
+
+    no_rev_count = demissionaires.loc[demissionaires['MTREV'] > 0]
+    print(f'Nombre de demissionaires avec un revenu: {len(no_rev_count)}')
+
+    print_sep()
+
+    cat_per_col(demissionaires, 'CDSEXE')
+    print()
+    cat_per_col(demissionaires, 'CDSITFAM')
+    print()
+    cat_per_col(demissionaires, 'CDMOTDEM')
+    print()
+    cat_per_col(demissionaires, 'CDCATCL')
+
+    hist_per_col(demissionaires, 'AGEDEM')
+    hist_per_col(demissionaires, 'ADH')
+    hist_per_col(demissionaires, 'ANNEEDEM')
+
+    print_sep()
+
+    print(f"Nombre de societaires demissionaires: {len(societaires.loc[societaires['DTDEM'] != '31/12/1900'])}")
+
+    print_sep()
+
+    numerical_cols = ['MTREV', 'NBENF', 'AGEAD', 'AGEDEM', 'ADH']
+    numerical_cols_demissionaires = demissionaires[numerical_cols].dropna()
+    scaler = StandardScaler()
+    df_scaled = scaler.fit_transform(numerical_cols_demissionaires)
+
+    pca = PCA(n_components=2)
+    pca_result = pca.fit_transform(df_scaled)
+
+    df_pca = pd.DataFrame(pca_result, columns=['PC1', 'PC2'])
+
+    print('Variance expliquée par chaque composante :', pca.explained_variance_ratio_)
+
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(x=df_pca["PC1"], y=df_pca["PC2"], alpha=0.5)
+    plt.xlabel('Première composante principale (PC1)')
+    plt.ylabel('Deuxième composante principale (PC2)')
+    plt.title('Projection des données sur les deux premières composantes')
+    plt.show()
+
+    pca = PCA()
+    pca.fit(df_scaled)
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(range(1, len(numerical_cols) + 1), np.cumsum(pca.explained_variance_ratio_), marker='o', linestyle='--')
+    plt.xlabel("Nombre de composantes")
+    plt.ylabel("Variance expliquée cumulée")
+    plt.title("Scree plot - Choix du nombre de composantes")
+    plt.show()
---
+++ readme.md
@@ -0,0 +1,41 @@
+Demissionnaires
+
+| Attribut   | Signification                                           |
+|------------|---------------------------------------------------------|
+| ID         | Identifiant unique (dans ce fichier)                    |
+| CDSEXE     | Code relatif au sexe                                    |
+| MTREV      | Montant des revenus                                     |
+| NBENF      | Nombre d’enfants                                        |
+| CDSITFAM   | Situation familiale                                     |
+| DTADH      | Date d’adhésion à l’organisme bancaire                  |
+| CDTMT      | Code représentant le statut du sociétaire (catégorie)   |
+| CDDEM      | Code de démission                                       |
+| DTDEM      | Date de démission                                       |
+| ANNEEDEM   | Année de démission                                      |
+| CDMOTDEM   | Motif de la démission (catégorie)                       |
+| CDCATCL    | Type de client (catégorie)                              |
+| AGEAD      | Âge du client à l’adhésion, en années                   |
+| RANGAGEAD  | Tranche d’âge du client à l’adhésion                    |
+| AGEDEM     | Âge du client à la démission, en années                 |
+| RANGAGEDEM | Tranche d’âge du client à la démission                  |
+| RANGDEM    | Date de la démission au format N AAAA (code puis année) |
+| ADH        | Durée de la période d’adhésion, en années               |
+| RANGADH    | Tranche de la durée de la période d’adhésion            |
+
+Societaires
+
+| Attribut | Signification                                          |
+|----------|--------------------------------------------------------|
+| ID       | Identifiant unique (dans ce fichier)                   |
+| CDSEXE   | Code relatif au sexe                                   |
+| DTNAIS   | Date de naissance                                      |
+| MTREV    | Montant des revenus                                    |
+| NBENF    | Nombre d’enfants                                       |
+| CDSITFAM | Situation familiale                                    |
+| DTADH    | Date d’adhésion à l’organisme bancaire                 |
+| CDTMT    | Code représentant le statut du sociétaire (catégorie)  |
+| CDMOTDEM | Motif de la démission, ou rien si non-démissionnaire   |
+| CDCATCL  | Type de client (catégorie)                             |
+| BPADH    | Signification inconnue                                 |
+| DTDEM    | Date de démission, ou 31/12/1900 si non-démissionnaire |
+
